b5149077ba0efc40efe157f8f43c2563d610f08d
5goto <2breal1984@gmail.com>
2023-02-26 13:35:48 +1000

    autorisation added

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 import bd_repo
-
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from autorisation import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'FDFFJFhfjdfjhugurhg434ff'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(id):
+    print("load user")
+    return UserLogin().fromDB(user_id=id)
 
 
 @app.route('/')
+@login_required
 def index():  # put application's code here
+    print(bd_repo.getUserByID("4"))
     return render_template('index.html', title="ProFlask")
 
 
 @app.route('/books', methods=["POST", "GET"])
+@login_required
 def books_table():  # put application's code here
     if request.method == "POST":
         print(request.form)
@@ -21,14 +32,22 @@ def books_table():  # put application's code here
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    if "userLogged" in session:
-        return redirect(url_for("books_table", username=session["userLogged"]))
-    elif request.method == "POST" and request.form["username"] == "admin" and request.form["psw"] == "123":
-        session["userLogged"] = request.form["username"]
-        return redirect(url_for("books_table", username=session["userLogged"]))
+    if request.method == "POST":
+        user = bd_repo.getUserByLogin(request.form['username'])
+        print(user)
+        if user and check_password_hash(user[0][2], request.form['psw']):
+            user_login = UserLogin().create(user)
+            login_user(user_login)
+            return redirect(url_for("books_table"))
+
     return render_template('login.html')
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
---
+++ autorisation.py
@@ -0,0 +1,23 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+import bd_repo
+
+class UserLogin:
+    def fromDB(self, user_id):
+        self.__user = bd_repo.getUserByID(user_id)
+        return self
+
+    def create(self, user):
+        self.__user = user
+        return self
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.__user[0][0])
+
